ed27cd5828f5b69e9fbf81813fc584133b830c39
Eddie Ruano <eddieruano@gmail.com>
2017-05-18 19:07:40 -0700

    all

--- DESI_DRV1.py
+++ DESI_DRV1.py
@@ -2,7 +2,7 @@
 # @Author: Eddie Ruano
 # @Date:   2017-05-01 05:14:54
 # @Last Modified by:   Eddie Ruano
-# @Last Modified time: 2017-05-18 19:05:51
+# @Last Modified time: 2017-05-18 19:07:34
 
 """
 Basic DESI Driver for Prototyping
@@ -31,7 +31,7 @@ GR_05 = 25
 # #BOUNCE IN MS# #
 bounceTime = 1000
 # #STATE# #
-state = "Speed0"
+state = "SpeedA"
 
 
 ### MAIN PROGRAM START ###
