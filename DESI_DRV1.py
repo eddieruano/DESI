1f0274a3a3f1bee076535a501c69866466b56580
Eddie Ruano <eddieruano@gmail.com>
2017-05-30 16:21:36 -0700

    checking start

--- DESI_DRV1.py
+++ DESI_DRV1.py
@@ -2,7 +2,7 @@
 # @Author: Eddie Ruano
 # @Date:   2017-05-01 05:14:54
 # @Last Modified by:   Eddie Ruano
-# @Last Modified time: 2017-05-30 14:32:24
+# @Last Modified time: 2017-05-30 16:21:13
 
 """
 Basic DESI Driver for Prototyping
@@ -12,8 +12,8 @@ import RPi.GPIO as GPIO
 import time
 #import Adafruit_MPR121.MPR121 as MPR121
 # #Buttons# #
-G_INSTART = 9
-G_INPAUSE= 10
+G_INSTART = 10
+G_INPAUSE= 9
 G_INSTOP = 11
 G_INSLOW = 5
 G_INMED = 6
@@ -167,7 +167,6 @@ def performS4(channel):
       print(state)
 def performStart(channel):
    print("start")
-   print(GR_START)
    GPIO.output(GR_START, GPIO.LOW)
    time.sleep(0.1)
    GPIO.output(GR_START, GPIO.HIGH)
@@ -176,6 +175,7 @@ def performStart(channel):
    time.sleep(0.1)
    GPIO.output(GR_ENTER, GPIO.HIGH)
    time.sleep(0.1)
+
 def performStop(channel):
    print("stop")
    GPIO.output(GR_PAUSE, GPIO.LOW)
