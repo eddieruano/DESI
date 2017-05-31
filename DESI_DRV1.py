520fb371668659f8402b2207874adf53ad27023a
Eddie Ruano <eddieruano@gmail.com>
2017-05-31 12:29:56 -0700

    alexa

--- DESI_DRV1.py
+++ DESI_DRV1.py
@@ -2,7 +2,7 @@
 # @Author: Eddie Ruano
 # @Date:   2017-05-01 05:14:54
 # @Last Modified by:   Eddie Ruano
-# @Last Modified time: 2017-05-31 12:28:13
+# @Last Modified time: 2017-05-31 12:29:51
 
 """
 Basic DESI Driver for Prototyping
@@ -144,7 +144,7 @@ def performS3(channel):
       print(state)
 def performS4(channel):
    global state
-   if state != "Speed0":
+   if state != "Speed4":
       # Trigger 1 twice
       GPIO.output(GR_03, GPIO.LOW)
       time.sleep(0.1)
