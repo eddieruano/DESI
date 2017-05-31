4545871a69dbae2b6782149b913b2696debd3356
Eddie Ruano <eddieruano@gmail.com>
2017-05-31 13:22:19 -0700

    Snowboy

--- DESI_DRV1.py
+++ DESI_DRV1.py
@@ -2,12 +2,12 @@
 # @Author: Eddie Ruano
 # @Date:   2017-05-01 05:14:54
 # @Last Modified by:   Eddie Ruano
-# @Last Modified time: 2017-05-31 12:29:51
+# @Last Modified time: 2017-05-31 13:21:58
 
 """
 Basic DESI Driver for Prototyping
 """
-#import sys
+import sys
 import RPi.GPIO as GPIO
 import time
 #import Adafruit_MPR121.MPR121 as MPR121
@@ -36,7 +36,7 @@ GR_ALEXA = 21
 # #BOUNCE IN MS# #
 bounceTime = 800
 # #STATE# #
-state = "Speed0"
+state = "Startup"
 
 ### MAIN PROGRAM START ###
 def main():
@@ -58,11 +58,15 @@ def main():
    print("In Main Loop:\n")
    while activeFlag:
       activeFlag = True
-      
+      if state = "Shutdown":
+         print("Cleaning GPIO..")
+         print("DESI Shutdown Complete.")
+         GPIO.cleanup()
+         sys.exit
    #Should not get here
 def performS0(channel):
    global state
-   if state != "Speed0":
+   if state != "Startup":
       # Trigger 1 twice
       GPIO.output(GR_00, GPIO.LOW)
       time.sleep(0.1)
@@ -168,25 +172,44 @@ def performS4(channel):
       state = "Speed4"
       print(state)
 def performStart(channel):
-   print("start")
-   GPIO.output(GR_START, GPIO.LOW)
-   time.sleep(0.1)
-   GPIO.output(GR_START, GPIO.HIGH)
-   time.sleep(0.1)
-   GPIO.output(GR_ENTER, GPIO.LOW)
-   time.sleep(0.1)
-   GPIO.output(GR_ENTER, GPIO.HIGH)
-   time.sleep(0.1)
+   if state == "Startup":
+      print("start")
+      GPIO.output(GR_START, GPIO.LOW)
+      time.sleep(0.1)
+      GPIO.output(GR_START, GPIO.HIGH)
+      time.sleep(0.1)
+      GPIO.output(GR_ENTER, GPIO.LOW)
+      time.sleep(0.1)
+      GPIO.output(GR_ENTER, GPIO.HIGH)
+      time.sleep(0.1)
+      performS0(channel)
+   else:
+      print("Already Started.")
+
 def performStop(channel):
-   print("stop")
-   GPIO.output(GR_PAUSE, GPIO.LOW)
-   time.sleep(0.1)
-   GPIO.output(GR_PAUSE, GPIO.HIGH)
-   time.sleep(0.1)
+   if state == "Speed0:
+      print("Shutting Down.")
+      GPIO.output(GR_OFF, GPIO.LOW)
+      time.sleep(0.1)
+      GPIO.output(GR_OFF, GPIO.HIGH)
+      time.sleep(0.1)
+      global state
+      state = "Shutdown"
+      print(state)
+   else:
+      print("Pausing...\n")
+      GPIO.output(GR_PAUSE, GPIO.LOW)
+      time.sleep(0.1)
+      GPIO.output(GR_PAUSE, GPIO.HIGH)
+      time.sleep(0.1)
+      global state
+      state = "Paused"
+      print(state)
 def initializeButtons(start, pause):
    GPIO.setup(start, GPIO.IN, pull_up_down=GPIO.PUD_UP)
    GPIO.setup(pause, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-   print("Buttons Complete.\n")
+   print("Buttons Complete.")
+   print("Start & Pause Set.\n")
 def initializeKnob(speed0, speed1, speed2, speed3, speed4, speed5):
    GPIO.setup(speed0, GPIO.IN, pull_up_down=GPIO.PUD_UP)
    GPIO.setup(speed1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
